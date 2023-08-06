fc522a0a44c86fba493fc0fba5de16333c64a6ec
DinahFitiavana <hei.dinah.3@gmail.com>
2023-08-06 09:28:52 +0300

    feat: "finish todo list with react"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [taskText, setTaskText] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [selectedTaskId, setSelectedTaskId] = useState(null);
+
+  const onChange = (e) => setTaskText(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (taskText.trim() !== '') {
+      if (selectedTaskId !== null) {
+        updateTask(selectedTaskId, taskText);
+        setSelectedTaskId(null);
+      } else {
+        addTask(taskText);
+      }
+      setTaskText('');
+    }
+  };
+
+  const addTask = (taskText) => {
+    const newTask = { id: Date.now(), text: taskText };
+    setTasks([...tasks, newTask]);
+  };
+
+  const updateTask = (taskId, newText) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, text: newText } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  const editTask = (taskId, taskText) => {
+    setSelectedTaskId(taskId);
+    setTaskText(taskText);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={taskText}
+          onChange={onChange}
+          placeholder="Ajouter une tâche"
+        />
+        <button type="submit">
+          {selectedTaskId !== null ? 'Update' : 'Ajouter +'}
+        </button>
+      </form>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.text}
+            <button onClick={() => editTask(task.id, task.text)}>Modifier</button>
+            <button onClick={() => deleteTask(task.id)}> X </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
